b6759a13625872c8138a9f65e627977b778ccb14
Christian Arevalo <tiancris200@gmail.com>
2024-01-09 21:17:48 -0500

    :alembic: Format code

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,69 @@
-import express, { Request, Response } from 'express';
-import multer from 'multer';
-import pinataSDK, {PinataPinOptions} from '@pinata/sdk';
-import dotenv from 'dotenv';
-import fs from 'fs';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import multer from "multer";
+import pinataSDK, { PinataPinOptions } from "@pinata/sdk";
+import dotenv from "dotenv";
+import fs from "fs";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
 app.use(express.json());
-app.use(cors({origin: '*'}));
+app.use(cors({ origin: "*" }));
 
 const pinataJWTKey = process.env.PINATA_JWT;
 
-const pinata = new pinataSDK({ pinataJWTKey: pinataJWTKey});
-const upload = multer({ dest: 'uploads/' });
+const pinata = new pinataSDK({ pinataJWTKey: pinataJWTKey });
+const upload = multer({ dest: "uploads/" });
 
+app.post("/uploadJson", async (req: Request, res: Response) => {
+  const { body } = req;
+  if (typeof body !== "object" || Array.isArray(body)) {
+    return res.status(400).send("Invalid JSON object.");
+  }
+  console.log(body);
 
-app.post('/uploadJson', async (req: Request, res: Response) => {
-    const { body } = req;
-    if (typeof body !== 'object' || Array.isArray(body)) {
-        return res.status(400).send('Invalid JSON object.');
-    }
+  const options: PinataPinOptions = {
+    pinataMetadata: {
+      name: "test.json",
+    },
+  };
 
-    const options: PinataPinOptions = {
-        pinataMetadata: {
-            name: 'test.json'
-        }
-    };
-
-    try {
-        const result = await pinata.pinJSONToIPFS(body, options);
-        res.send(res.send(result.IpfsHash));
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error uploading JSON to IPFS.');
-    }
+  try {
+    const result = await pinata.pinJSONToIPFS(body, options);
+    res.send(result.IpfsHash); // Send only the IPFS hash
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error uploading JSON to IPFS.");
+  }
 });
 
-
-app.post('/uploadFile', upload.single('file'), async (req: Request, res: Response) => {
+app.post(
+  "/uploadFile",
+  upload.single("file"),
+  async (req: Request, res: Response) => {
     if (!req.file) {
-        return res.status(400).send('No file uploaded.');
+      return res.status(400).send("No file uploaded.");
     }
 
     const readableStreamForFile = fs.createReadStream(req.file.path);
-    const options:PinataPinOptions= {
-        pinataMetadata: {
-            name: req.file.originalname
-        }
+    const options: PinataPinOptions = {
+      pinataMetadata: {
+        name: req.file.originalname,
+      },
     };
 
     try {
-        const result = await pinata.pinFileToIPFS(readableStreamForFile, options);
-        res.send(result.IpfsHash);
+      const result = await pinata.pinFileToIPFS(readableStreamForFile, options);
+      res.send(result.IpfsHash); // Send only the IPFS hash
     } catch (error) {
-        console.error(error);
-        res.status(500).send('Error uploading file to IPFS.');
+      console.error(error);
+      res.status(500).send("Error uploading file to IPFS.");
     }
-});
+  }
+);
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
